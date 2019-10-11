7629947d1a70117ff3487e12896e1ff0dcb14ef4
Andrea Moretti <andrea.moretti@zalando.de>
2019-10-11 12:18:43 +0200

    fix: use matchMedia for touch detection

--- src/rezponsive.jsx
+++ src/rezponsive.jsx
@@ -10,22 +10,10 @@ import MQ from 'mediaquery';
 
 export const RezponsiveContext = React.createContext({});
 
-// https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
 function isTouchDevice() {
-  if (
-    'ontouchstart' in window ||
-    (window.DocumentTouch && document instanceof DocumentTouch)
-  ) {
-    return true;
-  }
-
-  // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-  // https://git.io/vznFH
-  const prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-  const query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join(
-    '',
-  );
-  return window.matchMedia(query).matches;
+    // unsupported browser such as IE11 are desktop only and will correctly
+    // return false
+    return window.matchMedia('(pointer: coarse)').matches;
 }
 
 const isObject = a => a !== undefined && a !== null && typeof a === 'object';
